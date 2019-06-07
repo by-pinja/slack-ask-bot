131fdf4fa79d7a004e799ccee10d01508822edf1
Hekku2 <hekku2@gmail.com>
2019-06-07 13:38:34 +0300

    Added documentattion for getting answers

--- ConsoleTester/Options/QuestionnairesOption.cs
+++ ConsoleTester/Options/QuestionnairesOption.cs
@@ -2,7 +2,7 @@ using CommandLine;
 
 namespace ConsoleTester.Options
 {
-    [Verb("get", HelpText = "Returns list of created questionnaires")]
+    [Verb("questionnaires", HelpText = "Returns list of created questionnaires")]
     public class QuestionnairesOption
     {
 
