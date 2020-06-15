c99e09ce9ba37c6689fe33988f246a2802e07e2f
andrew <andrewpcfield@gmail.com>
2020-06-15 15:48:01 +0300

    Clean up

--- AcceptanceTests/Create-RunSettingsFile.ps1
+++ AcceptanceTests/Create-RunSettingsFile.ps1
@@ -1,11 +1,10 @@
 <#
     .SYNOPSIS
-    Retrieves necessary values from Azure and creates .runsettings-file
+    Retrieves necessary values from Azure and creates .runsettings-file. This should only be executed from the continuous integration.
     .DESCRIPTION
     The Azure environment should already exist and the webapp already deployed.
     .PARAMETER ResourceGroup
-    Name of the resource group that has the web app deployed.
-    
+    Name of the resource group that has the web app deployed.    
     .PARAMETER WebAppName
     Name of the target web app. If not set, resource group name is used.
 #>
